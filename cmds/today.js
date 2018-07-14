25bdf3e787e0aa00802174504212add43f233b5e
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2018-07-14 18:23:53 -0500

    [add] error handlign in today and forecast commands

--- cmds/today.js
+++ cmds/today.js
@@ -2,13 +2,14 @@
 
 const ora = require('ora')
 const getWeather = require('../utils/weather')
-// const getLocation = require('../utils/location')
+const getLocation = require('../utils/location')
+const error = require('../utils/error')
 
 module.exports = async (args) => {
   const spinner = ora().start()
 
   try {
-    const location = args.location || args.l // || await getLocation()
+    const location = args.location || args.l || await getLocation()
     const weather = await getWeather(location)
 
     spinner.stop()
@@ -17,6 +18,6 @@ module.exports = async (args) => {
     console.log(`\t${weather.condition.temp}°F - ${weather.condition.text}`)
   } catch (err) {
     spinner.stop()
-    console.error(err)
+    error(err.response.data.message, true)
   }
 }
