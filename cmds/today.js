0f2d58494f7534a7697fee812ab2cf91a1f0673f
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2018-07-14 14:37:51 -0500

    [add] getWeather function in command today

--- cmds/today.js
+++ cmds/today.js
@@ -1,5 +1,21 @@
 'use strict'
 
-module.exports = (args) => {
-  console.log('Today is sunny')
+const ora = require('ora')
+const getWeather = require('../utils/weather')
+
+module.exports = async (args) => {
+  const spinner = ora().start()
+
+  try {
+    const location = args.location || args.l
+    const weather = await getWeather(location)
+
+    spinner.stop()
+
+    console.log(`Current conditions in ${location}:`)
+    console.log(`\t${weather.condition.temp}°F - ${weather.condition.text}`)
+  } catch (err) {
+    spinner.stop()
+    console.error(err)
+  }
 }
